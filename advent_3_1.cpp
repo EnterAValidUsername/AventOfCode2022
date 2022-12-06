15e6682d8f8ce09fa171f8c1af7ddd26065d41e9
EnterAValidUsername <tommopagano@icloud.com>
2022-12-06 20:53:15 +0100

    Create advent_3_1.cpp
    the first idea of the possible solution of the third day, section one of Advent of Code 2022

---
+++ advent_3_1.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ifstream fin ("input.txt");
+
+int main() {
+	
+	string input;
+	int ans = 0;
+	
+	while (fin >> input) {
+		int N = input.size();
+		
+		map < int, char > m;
+		
+		for (int i = 0; i < N/2; i++) {
+			m[i] = input[i];
+		}
+		
+		for (int i = N/2; i < N; i++) {
+			if (m.count(input[i])) { // non entra mai qui
+				cout << "entrato: " << input[i] << endl;
+				if ((int)input[i] >= 65 and (int)input[i] <= 90) {
+					ans += ((int)input[i] - 65 + 26 + 1);
+					/* 
+					the ascii values of the letters between A and Z space between 65 and 90, so for the points calculation i have to subtract 65, the value
+					of the A character and to sum 26, as the first letters that the problem contemplates are the lowercase ones, and 1, because the first letter
+					of this sum doesn't value 26 but 27
+					*/
+				}
+				else if ((int)input[i] >= 97 and (int)input[i] <= 122) {
+					ans += ((int)input[i] - 97 + 1);
+					/* 
+					the ascii values of the letters between a and z here space between 97 and 122 and i'm proceeding the exact same way as i did above
+					*/
+				}
+				break; 
+				// this break grants me that after finding the letter paired in both the portions of the sack the calculation of this input stops, avoiding annoying
+				// errors and lowering a littel bit the complexity too
+			}
+		}
+	}
+	
+	cout << ans;
+
+	return 0;
+}
+
