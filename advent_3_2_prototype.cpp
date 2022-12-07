083d0942dbd49f9cddb0708a654ab420fcad8aa8
EnterAValidUsername <118724712+EnterAValidUsername@users.noreply.github.com>
2022-12-07 10:00:41 +0100

    Add files via upload

---
+++ advent_3_2_prototype.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+ifstream fin ("input.txt");
+
+main() {
+	string input1, input2, input3;
+	unordered_set < char > Elf1, Elf2, Elf3; // i figured out that sets are better than maps in this problem case and in the previous (3_1)
+	
+	while (fin >> input1 >> input2 >> input3) {
+		// posso farla in N per ogni 3 input presi
+	}
+	
+}
+
