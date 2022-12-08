a966b8b5e52382f9dcbb2ae02d24f89946b78be0
EnterAValidUsername <tommopagano@icloud.com>
2022-12-08 10:03:59 +0100

    Update advent_3_2_prototype.cpp
    correction of one of my previous mistakes, it was so obvious that i didn't even immagine that there could be the error

--- advent_3_2_prototype.cpp
+++ advent_3_2_prototype.cpp
@@ -5,26 +5,25 @@ using namespace std;
 ifstream fin ("input.txt");
 
 main() {
-	// da sempre 0, si vede che la condizione di verifica per la presenza degli oggetti negli zaini, interna al primo for, del while non è mai vera
 	string input1, input2, input3;
 	unordered_set < char > Elf1, Elf2, Elf3;
 	int ans = 0;
 	
 	while (fin >> input1 >> input2 >> input3) {
 		// filling my Elf's rucksacks
-		int N = Elf1.size();
+		int N = input1.size();
 		for (int i = 0; i < N; i++) {
 			if (!Elf1.count(input1[i])) {
 				Elf1.insert(input1[i]);
 			}
 		}
-		N = Elf2.size();
+		N = input2.size();
 		for (int i = 0; i < N; i++) {
 			if (!Elf2.count(input2[i])) {
 				Elf2.insert(input2[i]);
 			}
 		}
-		N = Elf3.size();
+		N = input3.size();
 		for (int i = 0; i < N; i++) {
 			if (!Elf3.count(input3[i])) {
 				Elf3.insert(input3[i]);
@@ -32,19 +31,28 @@ main() {
 		}
 		
 		N = Elf1.size();
-		unordered_set < char > :: iterator i; // an iterator is like a pointer which can only be used to iterate through STL structs
-		for (i = Elf1.begin(); i != Elf1.end(); i++) { // i'm assigning the value of the pointer which is pointing to the first element and forward to the end to the iterator i
-			if (Elf2.count(*i) && Elf3.count(*i)) { // if the value of the element which i is pointing toward is in the set then do...
-				if ((int) *i >= 65 and (int) *i <= 90) {
-					ans += ((int) *i - 65 + 26 + 1);
+		// auto here signifies: unordered_set < char > :: iterator i; // an iterator is like a pointer which can only be used to iterate through STL structs
+		for (auto i = Elf1.begin(); i != Elf1.end(); i++) { // i'm checking if any of the elements in Elf1 is also present in Elf2 and ELf3, more precisely:
+		// if the value of the cell i is pointing towards (in Elf1), is equal to any of the elements in Elf2 and Elf3 then //*calculate priority points*
+			if (Elf2.count(*i) && Elf3.count(*i)) {
+				if (*i >= 65 and *i <= 90) {
+					cout << (int) *i - 65 + 26 + 1 << ", sono tra 65 e 90; ";
+					ans += (int) *i - 65 + 26 + 1;
 				}
-				else if ((int) *i >= 97 and (int) *i <= 122) {
-					ans += ((int) *i - 97 + 1);
+				else if (*i >= 97 and *i <= 122) {
+					cout << (int) *i - 97 + 1 << ", sono tra 97 e 122; ";
+					ans += (int) *i - 97 + 1;
 				}
 				break; 
 			}
 		}
 	}
 	cout << ans;
+	
+	// it starts mistaking the ASCII conversions after the first one
+	/*
+	all of the numbers that you see in the calculations above referres to the conversion numbers from the
+	complete ascii table setting the values of a = 97, z = 112, A = 65, and Z = 90
+	*/
 }
 
