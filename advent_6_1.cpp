7c399b4c97223b84623ab3fd5357d8430d091006
EnterAValidUsername <118724712+EnterAValidUsername@users.noreply.github.com>
2022-12-14 10:08:58 +0100

    yes, there's nothing in it

---
+++ advent_6_1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+main() {
+	string input;
+	fin >> input;
+	
+	int N = input.size();
+	unordered_set < char > count;
+	queue < char > q;
+	
+	while (q.size() < 4) {
+		
+	}
+}
+
