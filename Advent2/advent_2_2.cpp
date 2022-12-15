77f828de9c4a01fa043a8b305d9d5e9e0d4fdbd7
EnterAValidUsername <118724712+EnterAValidUsername@users.noreply.github.com>
2022-12-15 20:15:10 +0100

    Organization

---
+++ Advent2/advent_2_2.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ifstream fin ("input.txt");
+
+int main() {
+	
+	char temp[2];
+	int total = 0;
+	
+	while (fin >> temp[0] and fin >> temp[1]) {
+		if (temp[0] == 'A') {
+			if (temp[1] == 'X') total += (3 + 0);
+			else if (temp[1] == 'Y') total += (1 + 3);
+			else total += (2 + 6);
+		}
+		else if (temp[0] == 'B') {
+			if (temp[1] == 'X') total += (1 + 0);
+			else if (temp[1] == 'Y') total += (2 + 3);
+			else total += (3 + 6);
+		}
+		else {
+			if (temp[1] == 'X') total += (2 + 0);
+			else if (temp[1] == 'Y') total += (3 + 3);
+			else total += (1 + 6);
+		}
+	}
+	
+	cout << total;
+
+	return 0;
+}
+
