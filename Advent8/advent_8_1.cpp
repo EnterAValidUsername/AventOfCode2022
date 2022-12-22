8ebfe7f474544498b053057f7a649087168cb4a6
EnterAValidUsername <tommopagano@icloud.com>
2022-12-22 17:19:31 +0100

    Finally a decent solution for Advent_8_1

--- Advent8/advent_8_1.cpp
+++ Advent8/advent_8_1.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 ifstream fin ("input.txt");
 
+/*
+	for clarity sake, in nested for loops i use:
+	i = +x (rows);
+	j = -y (columns);
+*/
+
 int main() {
-	// to avoid any inconvenience:
-	// - y = i
-	// + x = j
-	
 	string input;
 	vector < vector < pair < int, bool > > > grid;
 	int S;
@@ -17,73 +19,95 @@ int main() {
 		vector < pair < int, bool > > temp(S);
 		
 		for (int i = 0; i < S; i++) {
-			temp[i].first = (int)input[i];
+			temp[i].first = (int)input[i] - 48;
 			temp[i].second = false;
 		}
+		
 		grid.push_back(temp);
 	}
-	// grid of trees saved successfully, every tree now is an int
+	
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	int ans = 0;
-	ans += (S - 1) * 4; // all of the external trees are visible
 	
-	for (int i = 1; i < S - 1; i++) { // rows, left to right
-		int current_max = grid[i][0].first;
+	for (int i = 0; i < S; i++) {
+		grid[i][0].second = true; grid[i][S - 1].second = true;
+		ans += 2;
 		
-		for (int j = 1; j < S - 1; j++) {
+		int current_max = grid[i][0].first;
+		int max;
+		
+		for (int j = 1; j < S; j++) { // first and last already checked
+			if (grid[i][j].first > current_max) {
+				current_max = grid[i][j].first;
+				
+				if (!grid[i][j].second) {
+					grid[i][j].second = true; ans++;
+				}
+			}
+		}
+		
+		max = current_max;
+		current_max = grid[i][S - 1].first;
+		
+		
+		for (int j = (S - 1) - 1; j > 0; j--) { // i know that the first is already been set to true and that the last has been checked by the previous for loop
+			if (grid[i][j].first == max) break;
 			
 			if (grid[i][j].first > current_max) {
-				
 				current_max = grid[i][j].first;
-				grid[i][j].second = true;
-				ans++;
+				
+				if (!grid[i][j].second) {
+					grid[i][j].second = true; ans++;
+				}
 			}
 		}
 	}
 	
-	for (int i = 1; i < S - 1; i++) { // rows, right to left
-		int current_max = grid[i][S - 1].first;
+	// rows: left - right, right - left; it compiles correctly;
+	
+	for (int j = 0; j < S; j++) {
 		
-		for (int j = S - 2; j > 0; j--) {
-			
-			if (grid[i][j].first > current_max && !grid[i][j].second) {
-				
-				current_max = grid[i][j].first;
-				grid[i][j].second = true;
-				ans++;
-			}
+		if (!grid[0][j].second) {
+			grid[0][j].second = true; ans++;
 		}
-	}
-	
-	for (int j = 1; j < S - 1; j++) {
+		if (!grid[S - 1][j].second) {
+			grid[S - 1][j].second = true; ans++;
+		}
+		
 		int current_max = grid[0][j].first;
+		int max;
 		
 		for (int i = 1; i < S - 1; i++) {
-			
-			if (grid[i][j].first > current_max && !grid[i][j].second) {
-				
+			if (grid[i][j].first > current_max) {
 				current_max = grid[i][j].first;
-				grid[i][j].second = true;
-				ans++;
+				
+				if (!grid[i][j].second) {
+					grid[i][j].second = true; ans++;
+				}
 			}
 		}
-	}
-	
-	for (int j = 1; j < S - 1; j++) {
-		int current_max = grid[0][j].first;
 		
-		for (int i = S - 2; i > 0; i--) {
+		max = current_max;
+		current_max = grid[S - 1][j].first;
+		
+		for (int i = (S - 1) - 1; i > 0; i--) {
+			if (grid[i][j].first == max) break;
 			
-			if (grid[i][j].first > current_max && !grid[i][j].second) {
-				
+			if (grid[i][j].first > current_max) {
 				current_max = grid[i][j].first;
-				grid[i][j].second = true;
-				ans++;
+				
+				if (!grid[i][j].second) {
+					grid[i][j].second = true; ans++;
+				}
 			}
 		}
 	}
 	
 	cout << ans;
+	
+	// columns: top - bottom, bottom - top; it compiles correctly;
+	// bugged: the example is right, but the solution for their input is too low
 
 	return 0;
 }
